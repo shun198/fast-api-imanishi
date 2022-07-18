71370b831d0aec3882b8166953614d2bb630ed8f
shun198 <shunhiroseluvmri@gmail.com>
2022-07-18 19:18:12 +0900

    :rocket: include conversation room reservation page

--- conference-reserve/app.py
+++ conference-reserve/app.py
@@ -1,88 +1,124 @@
 import streamlit as st
 import datetime
-import random
 import requests
 import json
-
+import pandas as pd
 
 page = st.sidebar.selectbox("Choose your page", ["users", "rooms", "bookings"])
 
 if page == "users":
-    st.title("APIテスト画面(ユーザ)")
+    st.title("ユーザ登録画面")
     with st.form(key="user"):
-        user_id: int = random.randint(0, 100)
+        # user_id: int = random.randint(0, 100)
         username: str = st.text_input("ユーザ名", max_chars=20)
         data = {
-            "user_id": user_id,
+            # "user_id": user_id,
             "username": username
         }
-        submit_button = st.form_submit_button(label="リクエスト送信")
+
+        submit_button = st.form_submit_button(label="ユーザ登録")
 
     if submit_button:
-        st.write("## 送信データ")
-        st.json(data)
-        st.write("## レスポンス結果")
         url = "http://127.0.0.1:8000/users"
         response = requests.post(
             url,
             data=json.dumps(data),
         )
+        if response.status_code == 200:
+            st.success("ユーザ登録が完了しました")
         st.write(response.status_code)
         st.json(response.json())
 elif page == "rooms":
-    st.title("APIテスト画面(会議室)")
+    st.title("会議室登録画面")
     with st.form(key="room"):
-        room_id: int = random.randint(0, 10)
+        # room_id: int = random.randint(0, 10)
         room_name: str = st.text_input("会議室名", max_chars=20)
         capacity: int = st.number_input("定員", step=1)
         data = {
-            "room_id": room_id,
+            # "room_id": room_id,
             "room_name": room_name,
-            "capacity": capacity
+            "capacity": capacity,
         }
         submit_button = st.form_submit_button(label="リクエスト送信")
 
     if submit_button:
-        st.write("## 送信データ")
-        st.json(data)
-        st.write("## レスポンス結果")
         url = "http://127.0.0.1:8000/rooms"
         response = requests.post(
             url,
             data=json.dumps(data),
         )
+        if response.status_code == 200:
+            st.success("会議室登録が完了しました")
         st.write(response.status_code)
         st.json(response.json())
 elif page == "bookings":
-    st.title("APIテスト画面(予約)")
+    st.title("会議室予約画面")
+    # ユーザ一覧取得
+    url_users = "http://127.0.0.1:8000/users"
+    response = requests.get(url_users)
+    users = response.json()
+    # ユーザ名をキー,ユーザIDをバリューに
+    users_dict = {}
+    for user in users:
+        users_dict[user["username"]] = user["user_id"]
+
+    # 会議室一覧取得
+    url_rooms = "http://127.0.0.1:8000/rooms"
+    response = requests.get(url_rooms)
+    rooms = response.json()
+    rooms_dict = {}
+    for room in rooms:
+        rooms_dict[room["room_name"]] = {
+            "room_id": room["room_id"],
+            "capacity": room["capacity"],
+        }
+    st.write("## 会議室一覧")
+    df_rooms = pd.DataFrame(rooms)
+    df_rooms.columns = ["会議室名", "定員", "会議室ID"]
+    st.table(df_rooms)
+
     with st.form(key="booking"):
-        booking_id: int = random.randint(0, 100)
-        user_id: int = random.randint(0, 100)
-        room_id: int = random.randint(0, 10)
-        booked_num: int = st.number_input("予約人数", step=1)
+        username: str = st.selectbox("予約者名", list(users_dict.keys()))
+        room_name: str = st.selectbox("会議室名", list(rooms_dict.keys()))
+        booked_num: int = st.number_input("予約人数", step=1, min_value=1)
         date = st.date_input("日付を入力", min_value=datetime.datetime.today())
-        start_time = st.time_input(
-            "開始時刻", value=datetime.time(hour=9, minute=0))
-        end_time = st.time_input(
-            "終了時刻", value=datetime.time(hour=20, minute=0))
+        start_time = st.time_input("開始時刻", value=datetime.time(hour=9, minute=0))
+        end_time = st.time_input("終了時刻", value=datetime.time(hour=20, minute=0))
+        submit_button = st.form_submit_button(label="予約登録")
+
+    if submit_button:
+        user_id: int = users_dict[username]
+        room_id: int = rooms_dict[room_name]["room_id"]
+        capacity: int = rooms_dict[room_name]["capacity"]
+
         data = {
-            "booking_id": booking_id,
             "user_id": user_id,
             "room_id": room_id,
             "booked_num": booked_num,
-            "start_datetime": datetime.datetime(year=date.year, month=date.month, day=date.day, hour=start_time.hour, minute=start_time.minute).isoformat(),
-            "end_datetime": datetime.datetime(year=date.year, month=date.month, day=date.day, hour=end_time.hour, minute=end_time.minute).isoformat()
+            "start_datetime": datetime.datetime(
+                year=date.year,
+                month=date.month,
+                day=date.day,
+                hour=start_time.hour,
+                minute=start_time.minute,
+            ).isoformat(),
+            "end_datetime": datetime.datetime(
+                year=date.year,
+                month=date.month,
+                day=date.day,
+                hour=end_time.hour,
+                minute=end_time.minute,
+            ).isoformat(),
         }
-        submit_button = st.form_submit_button(label="リクエスト送信")
-
-    if submit_button:
-        st.write("## 送信データ")
-        st.json(data)
-        st.write("## レスポンス結果")
-        url = "http://127.0.0.1:8000/bookings"
-        response = requests.post(
-            url,
-            data=json.dumps(data),
-        )
-        st.write(response.status_code)
-        st.json(response.json())
+        # 定員以下の予約人数の場合
+        if booked_num <= capacity:
+            # 会議室の予約を行う
+            url = "http://127.0.0.1:8000/bookings"
+            response = requests.post(
+                url,
+                data=json.dumps(data),
+            )
+            if response.status_code == 200:
+                st.success("予約が完了しました")
+        else:
+            st.error(f"{room_name}の定員数は{capacity}人です")
--- conference-reserve/sql_app/models.py
+++ conference-reserve/sql_app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-from database import Base
+from .database import Base
 
 
 class User(Base):
@@ -18,10 +18,12 @@ class Room(Base):
 class Booking(Base):
     __tablename__ = "bookings"
     booking_id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey(
-        "users.user_id", ondelete="SET NULL"), nullable=False)
-    room_id = Column(Integer, ForeignKey(
-        "rooms.room_id", ondelete="SET NULL"), nullable=False)
+    user_id = Column(
+        Integer, ForeignKey("users.user_id", ondelete="SET NULL"), nullable=False
+    )
+    room_id = Column(
+        Integer, ForeignKey("rooms.room_id", ondelete="SET NULL"), nullable=False
+    )
     booked_num = Column(Integer)
     start_datetime = Column(DateTime, nullable=False)
     end_datetime = Column(DateTime, nullable=False)
