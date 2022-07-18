196bebb5ff8c5d7feec0739e2d7e1a9d77d28c29
shun198 <shunhiroseluvmri@gmail.com>
2022-07-18 16:14:48 +0900

    :rocket: divided main.py to main.py and schemas

--- conference-reserve/main.py
+++ conference-reserve/main.py
@@ -1,27 +1,5 @@
-import datetime
-from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
-
-
-class Booking(BaseModel):
-    booking_id: int
-    user_id: int
-    room_id: int
-    booked_num: int
-    start_datetime: datetime.datetime
-    end_datetime: datetime.datetime
-
-
-class User(BaseModel):
-    user_id: int
-    username: str = Field(min_length=3, max_length=12)
-
-
-class Room(BaseModel):
-    room_id: int
-    room_name: str = Field(min_length=3, max_length=12)
-    capacity: int
+from sql_app import User, Room, Booking
 
 
 app = FastAPI()
--- conference-reserve/sql_app/database.py
+++ conference-reserve/sql_app/database.py
@@ -3,3 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+engine = create_engine(
+    # sqliteのみconnect_argsに{'check_same_thread': False}を指定する
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
